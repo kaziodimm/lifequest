e03f6e9218e320f01a30e977dbe7bbaeff49dd4c
kaziodimm <kaziodimm@gmail.com>
2026-06-17 10:52:02 +0200

    Use require for Tailwind animate plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss";
-import animate from "tailwindcss-animate";
 
 const config: Config = {
   darkMode: ["class"],
@@ -55,7 +54,7 @@ const config: Config = {
       }
     }
   },
-  plugins: [animate]
+  plugins: [require("tailwindcss-animate")]
 };
 
 export default config;
