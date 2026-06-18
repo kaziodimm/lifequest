52fd683a8a4d58372956f53dfb9c70f2cb76615c
kaziodimm <kaziodimm@gmail.com>
2026-06-18 07:35:16 +0200

    Attach default tree theme to tree page

--- app/tree/page.tsx
+++ app/tree/page.tsx
@@ -2,12 +2,13 @@
 
 import { AppShell } from "@/components/app-shell";
 import { LifeTree } from "@/components/life-tree";
+import { defaultTreeThemeId } from "@/lib/tree-themes";
 import styles from "./tree-visuals.module.css";
 
 export default function TreePage() {
   return (
     <AppShell immersive>
-      <div className={styles.visualTreeSkin}>
+      <div className={styles.visualTreeSkin} data-tree-theme={defaultTreeThemeId}>
         <LifeTree />
       </div>
     </AppShell>
